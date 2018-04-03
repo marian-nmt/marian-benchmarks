7dfec1a703ce76b9002cec32213ca987cd0e54e6
Roman Grundkiewicz <rgrundki@exseed.ed.ac.uk>
2018-04-03 11:43:32 +0000

    Update batch sizes

--- benchmarks/training_wmt17_data/train-nematus-deep.sh
+++ benchmarks/training_wmt17_data/train-nematus-deep.sh
@@ -6,7 +6,7 @@ export LC_ALL=C.UTF-8
 MARIAN=../../tools/marian
 NEMATUS=../../tools/nematus.master
 DATA=$(realpath ../../models/wmt17_data)
-MODEL=$(realpath nematus.deep.110)
+MODEL=$(realpath nematus.deep)
 
 LOGFILE=$1
 shift
@@ -18,7 +18,7 @@ mkdir -p $MODEL
 test -e vocab.ende.yml || cat $DATA/all.bpe.{en,de} | $MARIAN/build/marian-vocab --max-size 36000 > vocab.ende.yml
 test -e vocab.ende.json || python -c 'import sys, yaml, json; json.dump(yaml.load(sys.stdin), sys.stdout, indent=4)' < vocab.ende.yml > vocab.ende.json
 
-CUDA_VISIBLE_DEVICES=$GPUS THEANO_FLAGS=mode=FAST_RUN,floatX=float32,device=cuda0=0.8 python $NEMATUS/nematus/nmt.py \
+CUDA_VISIBLE_DEVICES=$GPUS THEANO_FLAGS=mode=FAST_RUN,floatX=float32,device=cuda0,gpuarray.preallocate=0.8,scan.allow_output_prealloc=True python $NEMATUS/nematus/nmt.py \
     --model $MODEL/model.npz \
     --datasets $DATA/all.bpe.{de,en} \
     --dictionaries vocab.ende.json vocab.ende.json \
@@ -27,9 +27,10 @@ CUDA_VISIBLE_DEVICES=$GPUS THEANO_FLAGS=mode=FAST_RUN,floatX=float32,device=cuda
     --lrate 0.0001 \
     --optimizer adam \
     --maxlen 100 \
-    --batch_size 110 \
+    --batch_size 175 \
     --dispFreq 100 \
     --tie_decoder_embeddings \
+    --tie_encoder_decoder_embeddings \
     --layer_normalisation \
     --dec_base_recurrence_transition_depth 8 \
     --enc_recurrence_transition_depth 4 \
--- benchmarks/training_wmt17_data/train-nematus-shallow.sh
+++ benchmarks/training_wmt17_data/train-nematus-shallow.sh
@@ -18,7 +18,7 @@ mkdir -p $MODEL
 test -e vocab.ende.yml || cat $DATA/all.bpe.{en,de} | $MARIAN/build/marian-vocab --max-size 36000 > vocab.ende.yml
 test -e vocab.ende.json || python -c 'import sys, yaml, json; json.dump(yaml.load(sys.stdin), sys.stdout, indent=4)' < vocab.ende.yml > vocab.ende.json
 
-CUDA_VISIBLE_DEVICES=$GPUS THEANO_FLAGS=mode=FAST_RUN,floatX=float16,device=cuda0=0.8 python $NEMATUS/nematus/nmt.py \
+CUDA_VISIBLE_DEVICES=$GPUS THEANO_FLAGS=mode=FAST_RUN,floatX=float32,device=cuda0,gpuarray.preallocate=0.8,scan.allow_output_prealloc=True python $NEMATUS/nematus/nmt.py \
     --model $MODEL/model.npz \
     --datasets $DATA/all.bpe.{de,en} \
     --dictionaries vocab.ende.json vocab.ende.json \
@@ -28,7 +28,7 @@ CUDA_VISIBLE_DEVICES=$GPUS THEANO_FLAGS=mode=FAST_RUN,floatX=float16,device=cuda
     --lrate 0.0001 \
     --optimizer adam \
     --maxlen 100 \
-    --batch_size 155 \
+    --batch_size 250 \
     --dispFreq 100 \
     --tie_decoder_embeddings \
     --layer_normalisation \
